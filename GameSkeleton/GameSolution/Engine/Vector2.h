f98422f2895477096c9be479b136fbd8783f76cd
nickredmond <weeman220@yahoo.com>
2014-04-29 19:52:10 -0600

    Added Vector2.h and modified EntryPoint.cpp to include function callbacks.

---
+++ GameSkeleton/GameSolution/Engine/Vector2.h
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <math.h>
+
+using std::cout;
+using std::endl;
+
+struct Vector2{
+public:
+	float x, y;
+
+	Vector2(float xVal = 0.0f, float yVal = 0.0f) : x(x), y(y){
+		x = xVal;
+		y = yVal;
+	}
+
+	float dotProduct(const Vector2 &right){
+		float result = (x * right.x) + (y * right.y);
+	}
+
+	float length(){
+		return sqrt((x * x) + (y * y));
+	}
+
+	float lengthSquared(){
+		return (x * x) + (y * y);
+	}
+
+	Vector2 normalized(){
+		float len = length();
+
+		Vector2 result;
+		result.x = x / len;
+		result.y = y / len;
+
+		return result;
+	}
+
+	Vector2 perpCW(){
+		return Vector2(y, -x);
+	}
+
+	Vector2 perpCCW(){
+		return Vector2(-y,x);
+	}
+};
+
+//-- operators --//
+
+Vector2 operator+(const Vector2 &left, const Vector2 &right){
+	Vector2 result;
+	result.x = left.x + right.x;
+	result.y = left.y + right.y;
+
+	return result;
+}
+
+Vector2 operator-(const Vector2 &left, const Vector2 &right){
+	Vector2 result;
+	result.x = left.x - right.x;
+	result.y = left.y - right.y;
+
+	return result;
+}
+
+Vector2 operator*(const float &scalar, const Vector2 &right){
+	Vector2 result;
+	result.x = scalar * right.x;
+	result.y = scalar * right.y;
+
+	return result;
+}
+
+Vector2 operator*(const Vector2 &left, const float &scalar){
+	Vector2 result;
+	result.x = left.x * scalar;
+	result.y = left.y * scalar;
+
+	return result;
+}
+
+Vector2 operator/(const Vector2 &left, const float scalar){
+	Vector2 result;
+	result.x = left.x / scalar;
+	result.y = left.y / scalar;
+	
+	return result;
+}
+
+std::ostream& operator<<(std::ostream &stream, const Vector2 vec){
+	cout << "{ " << vec.x << ", " << vec.y << " }" << endl;
+	return stream;
+}
+
+//-- functions --//
+
+Vector2 LERP(Vector2 first, Vector2 second, float beta){
+	Vector2 result;
+	result.x = ((1.0f - beta) * first.x) + (beta * second.x);
+	result.y = ((1.0f - beta) * first.y) + (beta * second.y);
+	
+	return result;
+}
+
+float dotProduct(Vector2 &left, Vector2 &right){
+	return (left.x * right.x) + (left.y * right.y);
+}
--- GameSkeleton/TwoDCalculator/EntryPoint.cpp
+++ GameSkeleton/TwoDCalculator/EntryPoint.cpp
@@ -1,11 +1,66 @@
 #include "RenderUI.h"
 #include "Engine.h"
 
+#include "Vector2.h"
+
+Vector2 left, right, result;
+Vector2 original, normal, cwPerp, ccwPerp;
+Vector2 vec1, vec2, projectionVec, rejectionVec;
+Vector2 aVector, bVector, aMinusB, aVectorLerp, bVectorLerp, lerpResult;
+
+void basicVectorEqCallback(const BasicVectorEquationInfo& data){
+	left = data.scalar1 * Vector2(data.x1, data.y1);
+	right = data.scalar2 * Vector2(data.x2, data.y2);
+
+	result = data.add ? (left + right) : (left - right);
+}
+
+void perpenDataCallback(const PerpendicularData& data){
+	original = Vector2(data.x, data.y);
+	normal = original.normalized();
+	cwPerp = original.perpCW();
+	ccwPerp = original.perpCCW();
+}
+
+void dotProductCallback(const DotProductData& data){
+	vec1 = Vector2(data.v1i, data.v1j);
+	vec2 = Vector2(data.v2i, data.v2j);
+
+	if (data.projectOntoLeftVector){
+		float projScalar = dotProduct(vec1, vec2) / vec2.lengthSquared();
+		projectionVec = projScalar * vec2;
+
+		rejectionVec = vec1 - projectionVec;
+	}
+	else{
+		float projScalar = dotProduct(vec2, vec1) / vec1.lengthSquared();
+		projectionVec = projScalar * vec1;
+
+		rejectionVec = vec2 - projectionVec;
+	}
+}
+
+void lerpCallback(const LerpData& data){
+	aVector = Vector2(data.a_i, data.a_j);
+	bVector = Vector2(data.b_i, data.b_j);
+
+	aMinusB = bVector - aVector;
+	aVectorLerp = aVector * (1 - data.beta);
+	bVectorLerp = bVector * data.beta;
+
+	lerpResult = LERP(aVector, bVector, data.beta);
+}
+
 int main(int argc, char* argv[])
 {
 	Engine::Init();
 
 	RenderUI renderUI;
+
+	renderUI.setBasicVectorEquationData(basicVectorEqCallback, &left.x, &right.x, &result.x);
+	renderUI.setPerpendicularData(&original.x, &normal.x, &cwPerp.x, &ccwPerp.x, perpenDataCallback);
+	renderUI.setDotProductData(&vec1.x, &vec2.x, &projectionVec.x, &rejectionVec.x, dotProductCallback);
+	renderUI.setLerpData(&aVector.x, &bVector.x, &aMinusB.x, &aVectorLerp.x, &bVectorLerp.x, &lerpResult.x, lerpCallback);
 
 	if( ! renderUI.initialize(argc, argv))
 		return -1;
