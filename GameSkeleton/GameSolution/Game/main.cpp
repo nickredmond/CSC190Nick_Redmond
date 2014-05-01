188d3d70787f8dc831fc34493eab159a1264957c
nickredmond <weeman220@yahoo.com>
2014-05-01 11:08:31 -0600

    Fixed bugs with lab 1 (projecting onto wrong vector)

--- GameSkeleton/GameSolution/Game/main.cpp
+++ GameSkeleton/GameSolution/Game/main.cpp
@@ -3,5 +3,20 @@
 int main()
 {
 	Engine::sampleFunctionThatReturnsTrue();
+
+	//Core::Init( "Game Demo", SCREEN_WIDTH, SCREEN_HEIGHT);
+	//Core::RegisterUpdateFn(Update);
+	//Core::RegisterDrawFn(Draw);
+	//Core::GameLoop();
 }
 
+//bool Update(float dt){
+//	dt;
+//	return (Input::IsPressed(Input::KEY_ESCAPE));
+//}
+//
+//void Draw(Core::Graphics& graphics){
+//	graphics.setColor(RGB(255, 255, 255));
+//	graphics.DrawString(SCREEN_WIDTH/2 - 50, SCREEN_HEIGHT/2 - 20, "Hello World");
+//	grahpics.DrawLine(10, 10, 400, 300);
+//}
